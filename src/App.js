97ec3fe85dea6c385b6b5c93b587a4d6b875c17e
Олександр <90966662+GLFV@users.noreply.github.com>
2022-05-21 16:49:20 +0300

    second

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Button from './component/Button/Button'
 class App extends Component {
 	constructor() {
 		super()
-		this.state = {
-			nav_click: false,
-		}
+		this.state = {}
 	}
 	render() {
 		return (
@@ -16,7 +14,10 @@ class App extends Component {
 					<Navigation />
 				</div>
 				<div className='main'>
-					<Button />
+					<div className='btn-class'>
+						<h2>This website will pick a movie for you</h2>
+						<Button />
+					</div>
 				</div>
 			</div>
 		)
--- src/component/Button/Button.js
+++ src/component/Button/Button.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import './Button.css'
 const Button = () => {
-	return <div></div>
+	return (
+		<div className='center'>
+			<div className='btn-center'>
+				<h1 className='btn pointer pa2'>
+					<a href='#'>Top 100</a>
+				</h1>
+			</div>
+			<div>
+				<h1 className='btn pointer pa2'>
+					<a href='#'>Generate</a>
+				</h1>
+			</div>
+		</div>
+	)
 }
 
 export default Button
