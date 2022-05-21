b4a82190941a8540079476d2b046d368d5c60c97
Олександр <90966662+GLFV@users.noreply.github.com>
2022-05-21 11:42:17 +0300

    First

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+	return <div className='App'>Hello Ukraine!</div>
 }
 
-export default App;
+export default App
