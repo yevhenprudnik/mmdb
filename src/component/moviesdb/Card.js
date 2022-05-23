36742ff2da53b0afddcfec6141eccae7ae369578
yevhenprudnik <yevhenprudnik@gmail.com>
2022-05-23 17:23:48 +0300

    К

--- src/component/moviesdb/Card.js
+++ src/component/moviesdb/Card.js
@@ -1,12 +1,24 @@
 import React from "react";
 import swal from 'sweetalert'
-
-const Card = ({title}) => {
+import "./movie.css";
+const Card = ({title, subtitle, year, genre, topCast, short, image}) => {
     return(
-        <div className="tc bg-washed-blue dib br4 pa3 ma3 grow shadow-2">
-            <img className = "photo br4" src=""/>
-            <div>
-                <h2>{title}</h2>
+        <div className="dib br2 pa3 ma3 grow shadow-5 cardComponent">
+            <div className="container">
+                <div className = "image ph4 pt4 pb2">
+                    <img className = "photo br4" src={image}/>
+                </div>
+                <div>
+                    <h2>{title}</h2>
+                    <h3>{subtitle}</h3>
+                    <h3>{genre}</h3>
+                    <h4>{year}</h4>
+                    <h5>{short}</h5>
+                    <h3>{topCast}</h3>
+                </div>
+            </div>
+            <div className="pl4">
+                <button className="w-10 btn-grad pointer tc">Trailer</button>
             </div>
         </div>
     );
--- src/component/moviesdb/Movies.js
+++ src/component/moviesdb/Movies.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import Card from './Card';
+const getStr  = (arr) =>{
+    let str = "";
+    // arr.forEach(element => {
+    //     str += element + ", "
+    // });
+    for (let element of arr) {
+        str += element + ", ";
+    }
+    return str;
+}
 const Movies = ({movies}) => {
-    const cardComponent = movies.map((user, i) => {
+    const cardComponent = movies.map((movie, i) => {
         return (<Card 
         key = {i}
         id = {movies[i]._id} 
         title = {movies[i].title} 
+        subtitle = {movies[i].subtitle} 
+        year = {movies[i].year}
+        genre = {getStr(movies[i].genre)}
+        topCast = {getStr(movies[i].topCast)}
+        short = {movies[i].short}
+        image = {movies[i].image}
         // username = {movies[i].username}
         // email = {movies[i].email}
         // link = {movies[i].link}
