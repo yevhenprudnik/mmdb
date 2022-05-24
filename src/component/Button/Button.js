5c082a24b7365ccb4b74ffe98d2681c0dde263fa
yevhenprudnik <yevhenprudnik@gmail.com>
2022-05-24 22:25:19 +0300

    Пофіксив фон на телефоні

--- src/component/Button/Button.js
+++ src/component/Button/Button.js
@@ -4,8 +4,8 @@ import './Button.css'
 const Button = () => {
 	return (
 		<div className='center'>
-				<Scrollchor className='btn-center f3 btn pointer pa2 grow' to="#top100" animate={{offset: 10, duration: 600}} href='#top100'>Top 100</Scrollchor>
-				<Scrollchor className='btn-center f3 btn pointer pa2 grow' to="#top100" animate={{offset: 10, duration: 600}} href='#top100'>Generate</Scrollchor>
+				<Scrollchor className='btn-center f3 btn pointer pa2 grow' to="#top100" animate={{offset: -21, duration: 600}} href='#top100'>Top 100</Scrollchor>
+				<Scrollchor className='btn-center f3 btn pointer pa2 grow' to="#top100" animate={{offset: -21, duration: 600}} href='#top100'>Generate</Scrollchor>
 		</div>
 	)
 }
---
+++ src/component/picker/Picker.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./picker.css";
+class Picker extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+	render() {
+		return (
+            <div className="dib br2 pa3 ma3 shadow-5 cardComponent1">
+				
+            </div>
+		)
+	}
+}
+
+export default Picker
