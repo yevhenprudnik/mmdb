e5c6b67299e99b3dd4f498b9b4f4aecb88cd63ef
Олександр <90966662+GLFV@users.noreply.github.com>
2022-05-25 21:24:45 +0300

    Добавив хуйню якусь

--- src/App.js
+++ src/App.js
@@ -8,54 +8,60 @@ import Movies from './component/moviesdb/Movies'
 import Picker from './component/picker/Picker'
 
 const initialState = {
-	route: "home",
+	route: 'home',
 	signedIn: false,
 	username: '',
 	movies: [
 		{
-			title: "Hacksaw Ridge",
-			subtitle: "",
-			genre: ["Drama", "War movie"],
+			title: 'Hacksaw Ridge',
+			subtitle: '',
+			genre: ['Drama', 'War movie'],
 			IMDBraiting: 8.1,
 			Raiting: 0,
 			year: 2016,
-			topCast: ["Andrew Garfield", "Sam Worthington", "Teresa Palmer"],
-			director: "Mel Gibson",
+			topCast: ['Andrew Garfield', 'Sam Worthington', 'Teresa Palmer'],
+			director: 'Mel Gibson',
 			Runtime: 139,
-			image:"https://m.media-amazon.com/images/M/MV5BMjQ1NjM3MTUxNV5BMl5BanBnXkFtZTgwMDc5MTY5OTE@._V1_FMjpg_UX1000_.jpg",
-			trailer: "https://www.youtube.com/watch?v=s2-1hz1juBI&ab_channel=LionsgateMovies",
-			synopsis: "World War II American Army Medic Desmond T. Doss, who served during the Battle of Okinawa, refuses to kill people and becomes the first man in American history to receive the Medal of Honor without firing a shot.",
-			company: ["friends", "alone"],
-			mood: "neutral",
-			category: ["War movie", "Movies that may change the way you look at life"],
-		}
-]
+			image:
+				'https://m.media-amazon.com/images/M/MV5BMjQ1NjM3MTUxNV5BMl5BanBnXkFtZTgwMDc5MTY5OTE@._V1_FMjpg_UX1000_.jpg',
+			trailer:
+				'https://www.youtube.com/watch?v=s2-1hz1juBI&ab_channel=LionsgateMovies',
+			synopsis:
+				'World War II American Army Medic Desmond T. Doss, who served during the Battle of Okinawa, refuses to kill people and becomes the first man in American history to receive the Medal of Honor without firing a shot.',
+			company: ['friends', 'alone'],
+			mood: 'neutral',
+			category: [
+				'War movie',
+				'Movies that may change the way you look at life',
+			],
+		},
+	],
 }
 class App extends Component {
 	constructor() {
 		super()
 		this.state = initialState
 	}
-	onRouteChange = (route) => {
+	onRouteChange = route => {
 		if (route === 'signOut') {
 			this.setState(initialState)
 		}
-		this.setState({route: route});
+		this.setState({ route: route })
 	}
-	onUserChange = (username) => {
-		this.setState({username : username})
+	onUserChange = username => {
+		this.setState({ username: username })
 	}
 	onSignIn = () => {
-		this.setState({signedIn: true})
+		this.setState({ signedIn: true })
 	}
-	componentDidMount(){
+	componentDidMount() {
 		fetch('http://localhost:3001/getMovies', {
 			method: 'get',
-			headers: { 'Content-Type': 'application/json' }
+			headers: { 'Content-Type': 'application/json' },
 		})
 			.then(response => response.json())
 			.then(data => {
-				this.setState({movies: data})
+				this.setState({ movies: data })
 			})
 	}
 	render() {
@@ -63,30 +69,37 @@ class App extends Component {
 		return (
 			<div className='App'>
 				<div className='navbar'>
-					<Navigation signedIn={this.state.signedIn} onRouteChange = {this.onRouteChange}/>
+					<Navigation
+						signedIn={this.state.signedIn}
+						onRouteChange={this.onRouteChange}
+					/>
 				</div>
-				{
-					this.state.route == "signIn" 
-					? 
-					<SignIn onRouteChange={this.onRouteChange} onUserChange = {this.onUserChange} onSignIn = {this.onSignIn}/> 
-					:
-					this.state.route == "register"
-					?
-					<Register onRouteChange={this.onRouteChange} onUserChange = {this.onUserChange} onSignIn = {this.onSignIn}/> 
-					:
+				{this.state.route == 'signIn' ? (
+					<SignIn
+						onRouteChange={this.onRouteChange}
+						onUserChange={this.onUserChange}
+						onSignIn={this.onSignIn}
+					/>
+				) : this.state.route == 'register' ? (
+					<Register
+						onRouteChange={this.onRouteChange}
+						onUserChange={this.onUserChange}
+						onSignIn={this.onSignIn}
+					/>
+				) : (
 					<div className='main'>
-					<div className='btn-class pb7'>
-						<h2>This website will pick a movie for you</h2>
-						<Button />
+						<div className='btn-class pb7'>
+							<h2>This website will pick a movie for you</h2>
+							<Button />
+						</div>
+						<div className='pt4'>
+							<Picker onRouteChange={this.onRouteChange} />
+						</div>
+						<div className='pt4' id='top100'>
+							<Movies movies={this.state.movies} />
+						</div>
 					</div>
-					<div className='pt4'>
-						<Picker/>
-					</div>
-					<div className='pt4' id='top100'>
-						<Movies movies = {this.state.movies} />
-					</div>
-				</div>
-				}
+				)}
 			</div>
 		)
 	}
